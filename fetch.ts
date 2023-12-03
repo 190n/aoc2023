22523c03a445ccc909502beb5fc0cfac51fdd1f4
Ben Grant <benjamin.j.grant@gmail.com>
2023-12-03 15:04:37 -0800

    Clean up fetch script

--- fetch.ts
+++ fetch.ts
@@ -1,18 +1,16 @@
-import { load } from 'https://deno.land/std@0.208.0/dotenv/mod.ts';
-import cheerio from 'npm:cheerio';
+import { load } from 'npm:cheerio';
 
-const env = await load();
-const token = env.AOC_SESSION;
+const token = Deno.env.get('AOC_SESSION');
 const day = parseInt(Deno.args[0]);
 const headers = {
 	Cookie: `session=${token}`,
 };
 
 const source = await (await fetch(`https://adventofcode.com/2023/day/${day}`, { headers, })).text();
-const $ = cheerio.load(source);
+const $ = load(source);
 const exampleInput = $('pre > code').first().text();
 
 const input = await (await fetch(`https://adventofcode.com/2023/day/${day}/input`, { headers })).text();
 
-await Deno.writeFile(`inputs/example${day}.txt`, new TextEncoder().encode(exampleInput));
-await Deno.writeFile(`inputs/input${day}.txt`, new TextEncoder().encode(input));
+Deno.writeTextFileSync(`inputs/example${day}.txt`, exampleInput);
+Deno.writeTextFileSync(`inputs/input${day}.txt`, input);
